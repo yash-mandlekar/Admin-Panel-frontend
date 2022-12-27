3ab50bbd61bd2eafbbf6e30c33419c0ddf7f8ac2
yash-mandlekar <ymandlekar9826@gmail.com>
2022-12-27 11:34:31 +0530

    updating

--- src/Components/Axios/Axios.jsx
+++ src/Components/Axios/Axios.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const Axios = axios.create({
-    baseURL: 'http://localhost:4000/',
+    baseURL: 'http://52.66.186.55/',
     headers: {
         'Content-Type': 'application/json',
     },
